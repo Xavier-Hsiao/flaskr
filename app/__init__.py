07ddcac302288b21a3f20768122151db4a159b00
Xavier Hsiao <xavier.hsiao@0818XavierHsiao1.local>
2025-04-15 17:58:59 +0800

    add config class

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # the routes module needs to import the app variable defined in this script
 from app import routes;
