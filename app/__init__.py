88ba9dfff6345b487ba8de9a8d10aaa84c12e106
Xavier Hsiao <xavier.hsiao@0818XavierHsiao1.local>
2025-04-18 16:19:23 +0800

    feat: add User and Post database models and migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# the routes module needs to import the app variable defined in this script
-from app import routes;
+# the routes and models module needs to import the app variable defined in this script
+from app import routes, models;
